dfc0506a0120b7410f8b28992b223368cbc71faf
Maricom <akita447@gmail.com>
2019-01-30 19:47:30 +0900

    init

---
+++ chapter1/args/src/main.rs
@@ -0,0 +1,9 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("args.len() = {}", args.len());
+    for i in 0..args.len() {
+        println!("args[{}] = {}", i, args[i]); 
+    }
+}

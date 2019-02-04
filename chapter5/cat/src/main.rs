a517c96beae8674163ecca7a688263ae3f672b01
Maricom <akita447@gmail.com>
2019-02-04 22:05:15 +0900

    chapter5

---
+++ chapter5/cat/src/main.rs
@@ -0,0 +1,31 @@
+use std::io::{self, Write, Read};
+use std::fs::File;
+use std::env;
+use std::process;
+
+const BUFFER_SIZE: usize = 2048;
+
+fn main() {
+   let args: Vec<String> = env::args().collect(); 
+    if args.len() < 2 {
+        io::stderr().write(&format!("{}: file name not given\n", args[0]).into_bytes());
+        process::exit(1);
+    }
+    for arg in &args {
+        do_cat(arg);
+    }
+    process::exit(0);
+}
+
+fn do_cat(path: &str) -> io::Result<()> {
+    let mut buf: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
+    let mut f = File::open(path)?;
+    loop {
+        let n = f.read(&mut buf)?;
+        if n == 0 {
+            break;
+        }
+        io::stdout().write(&buf[..n])?;
+    }
+    Ok(())
+}

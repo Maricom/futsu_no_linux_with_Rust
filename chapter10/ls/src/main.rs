5304a5eba8d250892839db7802fe89e755645400
Maricom <akita447@gmail.com>
2019-03-01 22:02:58 +0900

    add -R option to ls

--- chapter10/ls/src/main.rs
+++ chapter10/ls/src/main.rs
@@ -1,22 +1,42 @@
 use std::env;
 use std::fs::read_dir;
+use std::path::Path;
+
+use getopts::Options;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    let mut opts = Options::new();
+    opts.optflag("R", "", "recursively list sub directories");
+
     if args.len() < 2 {
         println!("{}: no arguments", args[0]);
         std::process::exit(1);
     }
     
-    for arg in &args[1..] {
-        do_ls(arg);
+    let matches = opts.parse(&args[1..]).unwrap();
+    let r_flag = matches.opt_present("R");
+    let start_idx = if r_flag { 2 } else { 1 };
+
+    if args.len() <= start_idx {
+        println!("{}: no arguments", args[0]);
+        std::process::exit(1);
+    }
+
+    for arg in &args[start_idx..] {
+        do_ls(Path::new(arg), 0, r_flag);
     }
 }
 
-fn do_ls(path: &str) {
-    let dir = read_dir(path).expect(path);
+fn do_ls(path: &Path, layer: usize, r_flag: bool) {
+    let dir = read_dir(path).expect("invalid path");
     for entry in dir {
-        println!("{:?}", entry.unwrap().file_name());
+        let entry = entry.unwrap();
+        println!("{}|---{:?}", "|  ".repeat(layer), entry.file_name());
+        if r_flag && entry.file_type().unwrap().is_dir() {
+            do_ls(path.join(entry.file_name()).as_path(), layer+1, r_flag); 
+        }
     }
 }
     

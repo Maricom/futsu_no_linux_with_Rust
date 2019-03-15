19c81a3dad668ece281118efe9a66c751552d7b9
Maricom <akita447@gmail.com>
2019-03-15 12:48:20 +0900

    add pipe to shell

--- chapter12/shell/src/main.rs
+++ chapter12/shell/src/main.rs
@@ -1,7 +1,8 @@
 use std::io::{stdin, stdout, Write};
 use std::ffi::CString;
+use std::os::unix::io::RawFd;
 
-use nix::unistd::{fork, ForkResult, execv};
+use nix::unistd::{close, pipe, dup2, fork, ForkResult, execv};
 use nix::sys::wait::waitpid;
 
 fn main() {
@@ -10,22 +11,49 @@ fn main() {
 
     print_dollar();
     while let Ok(_) = stdin.read_line(&mut buf) {
+        let commands: Vec<&str> = buf.split('|').collect(); 
         
-        let args: Vec<CString> = buf.split_whitespace()
-            .map(|a| CString::new(a).unwrap())
-            .collect();
+        let mut pids = Vec::new();
+        let mut fds = Vec::new();
+        for i in 0..commands.len() {
+            fds.push(pipe().unwrap());
+        }
 
-        match fork() {
-            Ok(ForkResult::Child) => {
-                if let Err(e) = execv(&args[0], &args) {
-                    println!("{}", e);
-                }
-                break;
-            },
-            Ok(ForkResult::Parent { child }) => {
-                waitpid(child, None);
-            },
-            Err(e) => println!("{}", e),
+        for i in 0..commands.len() { 
+            let args: Vec<CString> = commands[i].split_whitespace()
+                .map(|s| CString::new(s).unwrap())
+                .collect();
+
+            match fork() {
+                Ok(ForkResult::Child) => {
+                    if i > 0 {
+                        close(0).unwrap();
+                        dup2(fds[i-1].0, 0);
+                    }
+                    
+                    if i < commands.len()-1 {
+                        close(1).unwrap();
+                        dup2(fds[i].1, 1); 
+                    }
+                    
+                    close_fds(fds);
+
+                    if let Err(e) = execv(&args[0], &args) {
+                        println!("{}", e);
+                    }
+                    std::process::exit(1);
+                },
+                Ok(ForkResult::Parent { child }) => {
+                    pids.push(child);
+                },
+                Err(e) => println!("{}", e),
+            }
+        }
+
+        close_fds(fds);
+
+        for pid in pids {
+            waitpid(pid, None);
         }
 
         buf.clear();
@@ -37,4 +65,11 @@ fn print_dollar() {
     let mut o = stdout();
     o.write("$ ".as_bytes());
     o.flush();
+}
+
+fn close_fds(fds: Vec<(RawFd, RawFd)>) {
+    for (i, o) in fds {
+        close(i).unwrap();
+        close(o).unwrap();
+    }
 }
